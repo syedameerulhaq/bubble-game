69b2e4fba0c68a2252fd20371fc042a2f630be15
syedameerulhaq <ameerulhaqyou@gmail.com>
2023-10-07 11:37:42 +0530

    HTML CSS

---
+++ scripts.js
@@ -0,0 +1,13 @@
+ var bubbleDiv = " ";
+ for(var i=1;i<=200;i++){
+    var randomNumber = Math.floor(Math.random() * 10) + 1
+    bubbleDiv +=`<div id="bubble"><h3>${randomNumber}</h3></div>`;
+
+ }
+ document.querySelector(".pbottom").innerHTML = bubbleDiv ;
+ document.querySelector("#bubble").addEventListener("click",function(temp){
+   document.querySelector("#bubble").style.backgroundColor="black";
+ }) ;
+
+ 
+
